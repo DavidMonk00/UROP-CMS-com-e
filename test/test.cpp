3f99b8399fb65832d7bb7791eaeda7e3fb5deb43
David Monk <d.g.monk0@gmail.com>
2017-06-23 13:01:05 +0100

    Started work on parser

--- test/test.cpp
+++ test/test.cpp
@@ -1,29 +1,87 @@
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+#include <boost/spirit/include/support_utree.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/assert.hpp>
 #include <iostream>
-#include <stdio.h>
-#include <boost/array.hpp>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <map>
+#include <stdlib.h>
 
-class Animal {
-public:
-  int x;
-  Animal(void) {x = 3;}
-  void printInt(void) {printf("%d\n", x);}
-  virtual void eat(void) {printf("Food.\n");}
+template <typename P, typename T>
+void test_parser_attr(char const* input, P const& p, T& attr, bool full_match = true) {
+   using boost::spirit::qi::parse;
+   char const* f(input);
+   char const* l(f + strlen(f));
+   if (parse(f, l, p, attr) && (!full_match || (f == l))) {
+   } else {
+      std::cout << "Parse failed." << std::endl;
+      std::cout << input << std::endl;
+      exit(-1);
+   }
+}
+
+namespace qi = boost::spirit::qi;
+namespace ascii = boost::spirit::ascii;
+
+template <typename Iterator>
+struct units_and_powers : qi::grammar<Iterator, std::map<std::string, std::string>()> {
+   units_and_powers() : units_and_powers::base_type(query) {
+        query =  pair >> *((qi::lit(';') | '&') >> pair);
+        pair  =  unit >> -('^' >> power);
+        unit  =  qi::char_("a-zA-Z_") >> *qi::char_("a-zA-Z_0-9");
+        power = +qi::char_("a-zA-Z_0-9");
+    }
+    qi::rule<Iterator, std::map<std::string, std::string>()> query;
+    qi::rule<Iterator, std::pair<std::string, std::string>()> pair;
+    qi::rule<Iterator, std::string()> unit, power;
 };
-class Cat : public Animal {
-public:
-  Cat(int a) {x = a;}
-  void eat(void) {printf("Rat.\n");}
-};
 
-void foo(Animal* a) {a->eat();}
+std::string parseUnit(std::string unit) {
+   using qi::symbols;
+   symbols<char, std::string> sym;
+   sym.add
+      ("m","metres")
+      ("K","kelvin")
+      ("s","seconds")
+   ;
+   std::string i = "Error";
+   test_parser_attr(unit.c_str(), sym, i);
+   return i;
+}
 
-using namespace std;
-int main(){
-  Animal* a = new Animal;
-  Animal* c = new Cat(2);
-  foo(a);
-  foo(c);
-  a->printInt();
-  c->printInt();
-  return 0;
+void splitUnits(std::string units) {
+   using qi:: char_;
+   using qi:: parse;
+   std::vector<std::string> unit;
+   parse(units.begin(), units.end(), *(char_), unit);
+   for(int i=0; i<unit.size(); ++i) {
+      std::cout << unit[i] << ' ';
+   }
+   std::cout << std::endl;
+}
+
+int main(void) {
+   using qi:: char_;
+   using qi::double_;
+   using qi:: parse;
+   std::string t("31.5ms^-1");
+   double value;
+   std::string units;
+   parse(t.begin(), t.end(), double_ >> *char_, value, units);
+   printf("Value: %.02f\nUnits: %s\n", value, units.c_str());
+   splitUnits(units);
+   std::string input("s^3;m;K^2");  // input to parse
+   std::string::iterator begin = input.begin();
+   std::string::iterator end = input.end();
+   units_and_powers<std::string::iterator> p;    // create instance of parser
+   std::map<std::string, std::string> m;        // map to receive results
+   bool result = qi::parse(begin, end, p, m);
+   std::cout << m["s"] << '\n';   // returns true if successful
+   return 0;
 }
