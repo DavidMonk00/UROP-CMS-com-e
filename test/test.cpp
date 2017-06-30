279d92520e4fc44b001416160543d7fc881da39f
David Monk <d.g.monk0@gmail.com>
2017-06-30 15:21:41 +0100

    Reworked text parser in test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -87,6 +87,55 @@ public:
     }
 };
 
+std::pair<double,int> foo(std::string input) {
+   std::pair<double, int> number;
+   using qi:: char_;
+   using qi::double_;
+   using qi:: parse;
+   std::string units;
+   using boost::spirit::qi::symbols;
+   symbols<char, int> unit_symbols;
+   unit_symbols.add
+      ("%", 1)
+      ("V", 2)
+      ("A", 3)
+      ("s", 4)
+      ("Hz",5)
+      ("K", 6)
+      ("b/s",7)
+   ;
+   parse(input.begin(), input.end(), double_ >> *char_, number.first, units);
+   if (units.length() == 0) {
+      number.second = 0;
+      return number;
+   } else {
+      std::vector<char> pre = {'p','n','u','m','k','M','G','T','P','E'};
+      auto lambda = [](std::string s, std::vector<char> pre){bool in = false;for (auto i : pre) {if (s.c_str()[0] == i) {in = true;}};return in;};
+      if (lambda(units, pre)) {
+         symbols<char, int> sym;
+         sym.add
+            ("p", -12)
+            ("n", -9)
+            ("u", -6)
+            ("m", -3)
+            ("k", 3)
+            ("M", 6)
+            ("G", 9)
+            ("T", 12)
+            ("P", 15)
+            ("E", 18)
+         ;
+         int i;
+         test_parser_attr(units.substr(0,1).c_str(), sym, i);
+         number.first = number.first*std::pow(10,i);
+         test_parser_attr(units.substr(1, units.length()).c_str(), unit_symbols, number.second);
+      } else {
+         test_parser_attr(units.c_str(), unit_symbols, number.second);;
+      }
+      return number;
+   }
+}
+
 int main() {
    phys_quant m;
    std::string units("9.81 kg m^2 s^-2");
@@ -133,26 +182,27 @@ int main() {
    quantity<nanosecond> t(1.0 * si::seconds);
    std::cout << t << std::endl;
 
-   using boost::spirit::qi::symbols;
-   symbols<char, int> sym;
-   sym.add
-      ("p", -12)
-      ("n", -9)
-      ("u", -6)
-      ("m", -3)
-      ("k", 3)
-      ("M", 6)
-      ("G", 9)
-      ("T", 12)
-      ("P", 15)
-      ("E", 18)
-   ;
-   double a = 3.0;
-   int i;
-   test_parser_attr("E", sym, i);
-   std::cout << 3*std::pow(10,i) << std::endl;
-
-
+   std::string str("2.1");
+   std::pair<double,int> pair = foo(str);
+   boost::variant<double,
+                  quantity<electric_potential>,
+                  quantity<current>,
+                  quantity<si::time> > var;
+   switch (pair.second) {
+      case 2:
+         var = pair.first*volts;
+         break;
+      case 3:
+         var = pair.first*amperes;
+         break;
+      case 4:
+         var = pair.first*seconds;
+         break;
+      case 1:
+      default:
+         var = pair.first;
+   }
+   std::cout << var << std::endl;
    //std::cout << vec[0] << '\n';
    /*std::pair<double, std::vector<std::pair<std::string, int> > > value;
    value = getValue("9.81 kg m s^-2");
