bf52e82f4f11ebdd2e1d92aaebb722606b0ca645
David Monk <d.g.monk0@gmail.com>
2017-07-04 10:40:31 +0100

    Build #004

--- src/SEMA.cpp
+++ src/SEMA.cpp
@@ -33,13 +33,11 @@ int main(int argc, char* argv[]) {
    }
    units_variant x = bus->read("seconds");
    cout << x << endl;
-   //printf("%X\n", x);
-   /*x = d.read("temperature");
+   x = bus->read("temperature");
    cout << x << endl;
    quantity<temperature> t = 61*kelvin;
-   d.write("SRAM1", t);
-   x = d.read("SRAM1");
-   //printf("%d\n", x);
-   cout << x << endl;*/
+   bus->write("SRAM1", t);
+   x = bus->read("SRAM1");
+   cout << x << endl;
    return 0;
 }
