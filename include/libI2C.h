37bb41cd5c8895e47b9b05a131fff4266150aba2
David Monk <d.g.monk0@gmail.com>
2017-07-19 17:03:57 +0100

    Added python call to raw I2C class to allow easier debugging

--- include/libI2C.h
+++ include/libI2C.h
@@ -7,3 +7,17 @@
 #include "ATCABoard.h"
 
 namespace py = pybind11;
+
+class I2CRaw {
+private:
+   uint8_t buffer;
+   I2C_base* i2c;
+   bool checkAvailability(void);
+   void requestBus(void);
+   void setFanOut(uint8_t buses);
+public:
+   I2CRaw(void);
+   ~I2CRaw();
+   int read(uint32_t address, uint32_t start_point);
+   void write(uint32_t address, uint8_t buffer, uint32_t start_point);
+};
